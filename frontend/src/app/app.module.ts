54d9f9f97f88c48f02952f05b4cdf2761632737c
max.weis <max.weis@openknowledge.de>
2020-08-05 18:24:24 +0200

    add remove button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,6 +9,8 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     NoopAnimationsModule,
     MatToolbarModule,
     MatTableModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/todolist/todolist.component.ts
+++ frontend/src/app/todolist/todolist.component.ts
@@ -9,7 +9,7 @@ import {Todo} from "../shared/todo";
 })
 export class TodolistComponent implements OnInit {
 
-  displayedColumns: string[] = ['status','id', 'name', 'dueDate'];
+  displayedColumns: string[] = ['status','id', 'name', 'dueDate','action'];
 
   dataSource: Todo[];
 
